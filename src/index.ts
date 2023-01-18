78ae8c63a4767006ab141f2a5831590fd4f0d4fe
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-01-18 15:11:26 +0500

    Git Init

--- src/index.ts
+++ src/index.ts
@@ -16,3 +16,64 @@ async function welcome() {
 };
 
 await welcome();
+
+let convertion = {
+    "PKR": {
+        "USD": 0.0044,
+        "GBP": 0.0037,
+        "PKR": 1
+    },
+    "USD": {
+        "USD": 1.21,
+        "PKR": 220.79,
+        "GBP": 1
+    },
+    "GBP": {
+        "PKR": 225.50,
+        "GBP": 0.83,
+        "USD": 1
+    }
+};
+
+interface userInput {
+    fromCurrency: "PKR" | "USD" | "GBP",
+    toCurrency: "PKR" | "USD" | "GBP",
+    amount: number
+};
+
+const answer: userInput = await inquirer.prompt([
+    {
+        name: "fromCurrency",
+        type: "list",
+        choices: ["PKR", "USD", "GBP"],
+        message: "Select Your Currency: \n"
+    },
+    {
+        name: "toCurrency",
+        type: "list",
+        choices: ["PKR", "USD", "GBP"],
+        message: "Select Your Convert Currency: \n"
+    },
+    {
+        name: "amount",
+        type: "number",
+        message: "Enter Your Convertion Amount: \n",
+        validate: (answer) => {
+            if (isNaN(answer)) {
+                return "Please Enter A Number";
+            }
+            return true;
+        }
+    },
+]);
+
+const { fromCurrency, toCurrency, amount } = answer;
+
+if (fromCurrency && toCurrency && amount) {
+
+    let result = convertion[fromCurrency][toCurrency] * amount;
+    console.log(chalk.cyan(`Your Convertion From ${fromCurrency} To ${toCurrency} Is ${result}`));
+
+} else {
+    console.log(chalk.red("Invalid Inputs"));
+}
